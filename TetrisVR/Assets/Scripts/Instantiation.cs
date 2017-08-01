8506c257146a0c040b2a59f725c5530cfd6d4ae9
killycal <caleb.lawrence.45@gmail.com>
2017-08-01 01:03:32 -0500

    Added Scripts

--- TetrisVR/Assets/Scripts/Instantiation.cs
+++ TetrisVR/Assets/Scripts/Instantiation.cs
@@ -1,16 +1,43 @@
 ﻿using UnityEngine;
+using System.Collections;
 public class Instantiation : MonoBehaviour {
 	public GameObject O,I,J,L,Z,S,T;
-	public Transform one,two,three,four,five,six,seven,eight,nine,ten;
+	public Vector3 one,two,three,four,five,six,seven,eight,nine,ten;
+	private Time time;
+	private IEnumerator schedule;
 	void Start() {
-		/*for (int y = 0; y < 5; y++) {
-			for (int x = 0; x < 5; x++) {
-				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		schedule=generate ();
+		StartCoroutine (schedule);
+
+	}
+	public IEnumerator generate(){
+		//one=GameObject.Find("10").transform;
+		yield return new WaitForSeconds (3);
+		Instantiate(O, one, Quaternion.identity);
+		yield return new WaitForSeconds (1);
+		//Instantiate(I, two, Quaternion.identity);
+
+		Instantiate(O, four, Quaternion.identity);
+		yield return new WaitForSeconds (1);
+		Instantiate(J, three, Quaternion.identity);
+		yield return new WaitForSeconds (1);
+		//Instantiate(I, five, Quaternion.identity);
+		Instantiate(I, six, Quaternion.identity);
+		yield return new WaitForSeconds (1);
+		Instantiate(I, seven, Quaternion.identity);
+		yield return new WaitForSeconds (1);
+		Instantiate(I, eight, Quaternion.identity);
+		yield return new WaitForSeconds (1);
+		Instantiate(I, nine, Quaternion.identity);
+		yield return new WaitForSeconds (1);
+		Instantiate(I, ten, Quaternion.identity);
+		yield return new WaitForSeconds (1);
+	}
+}
+
+/*for (int y = 0; y < 5; y++) {	for (int x = 0; x < 5; x++) {
+ * GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 				cube.AddComponent<Rigidbody>();
 				cube.transform.position = new Vector3(x, y, 0);
 			}
-		}*/one=GameObject.Find("10").transform;
-		print (one);
-		Instantiate(O, one.position, Quaternion.identity, one);
-	}
-}
+		}*/
---
+++ TetrisVR/Assets/Scripts/LineDetection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDetection : MonoBehaviour {
+	private int total=0;
+	private bool destroy=false;
+	void OnTriggerEnter(Collider other) {
+		total++;
+		if (total == 10) {
+			destroy = true;
+			print (destroy);
+		}
+	}
+	void OnTriggerExit(Collider other) {
+		if (destroy == false) {
+			total--;
+		}
+	}
+	void OnTriggerStay(Collider other){
+		if (destroy == true) {
+			Destroy(other.gameObject);
+			if (total !=0) {
+				total--;
+				print (total);
+			}
+			if (total==0) {
+				destroy = false;
+				print (destroy);
+			}
+		}
+	}
+}
